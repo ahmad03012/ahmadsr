db772d9da2c9706e2bd8eb3b6b706a292d29bdb3
ahmad03012 <140637471+ahmad03012@users.noreply.github.com>
2023-07-26 09:54:02 -0700

    config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", 29565918))
+API_HASH = getenv("API_HASH", "031c82d93ff3cb7d9711ba2ec4d4a9ec")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN", "6567617162:AAHjMwCr1jRIO_IGIb3JfNB-ihGJ_giyQWU")
